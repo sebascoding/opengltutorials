3d854ec9edeb5902fd4ff42475da39bdf9d1de5a
Sebastian Alvarez <sebascoding@gmail.com>
2016-07-04 13:42:29 +0200

    Added SOIL library

--- compile.sh
+++ compile.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-g++ -std=c++11 -L/usr/local/lib -lGLEW -lGLU -lm -lGL -lm -lpthread -ldl -ldrm -lXdamage -lX11-xcb -lxcb-glx -lxcb-dri2 -lxcb-dri3 -lxcb-present -lxcb-sync -lxshmfence -lglfw -lrt -lm -ldl -lXrandr -lXinerama -lXxf86vm -lXext -lXcursor -lXrender -lXfixes -lX11 -lpthread -lxcb -lXau -lXdmcp $1
+g++ -std=c++11 -L/usr/local/lib -lGLEW -lGLU -lm -lGL -lm -lpthread -ldl -ldrm -lXdamage -lX11-xcb -lxcb-glx -lxcb-dri2 -lxcb-dri3 -lxcb-present -lxcb-sync -lxshmfence -lglfw -lrt -lm -ldl -lXrandr -lXinerama -lXxf86vm -lXext -lXcursor -lXrender -lXfixes -lX11 -lpthread -lxcb -lXau -lXdmcp -lSOIL $1
